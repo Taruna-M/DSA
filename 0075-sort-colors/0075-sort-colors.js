f5b479784bf2060c586d515fc76c7eaf62e20a3a
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2024-12-22 23:31:16 +0530

    Time: 0 ms (100%), Space: 49.1 MB (72.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,5 +3,22 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    return nums.sort((a,b)=>a-b)
+    const n = nums.length;
+    let zero = 0; two = n-1; current = 0;
+    while (current<=two){
+        if (nums[current]===2){
+            const temp = nums[two];
+            nums[two] = nums[current];
+            nums[current] = temp;
+            two--;
+        }
+        else if (nums[current]===0){
+            const temp = nums[zero];
+            nums[zero] = nums[current];
+            nums[current] = temp;
+            zero++;
+            current++;
+        }
+        else current++;
+    }
 };
