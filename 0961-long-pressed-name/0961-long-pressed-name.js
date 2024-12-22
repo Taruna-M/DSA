470e6f46901a1047205a4e2da10313c479dba653
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2024-12-22 22:47:46 +0530

    Time: 0 ms (100%), Space: 49.4 MB (73.24%) - LeetHub

--- 0961-long-pressed-name/0961-long-pressed-name.js
+++ 0961-long-pressed-name/0961-long-pressed-name.js
@@ -6,24 +6,13 @@
 var isLongPressedName = function(name, typed) {
     const n1 = name.length, n2 = typed.length
     let i = 0, j = 0;
-    while (i<n1 && j<n2){
-        if (name[i]===typed[j]){
-        let count1 = 1;
-        while (i<n1-1 && name[i]===name[i+1]) {
-            count1++;
+    while (j<n2){
+        if (i<n1 && name[i]===typed[j]) {
             i++;
-        }
-        i++;
-        let count2 = 1;
-        while (j<n2-1 && typed[j]===typed[j+1]) {
-            count2++;
             j++;
         }
-        j++;
-        if (count1>count2) return false;
-        }
+        else if (j>0 && typed[j]===typed[j-1]) j++;
         else return false;
     }
-    if (i<n1 || j<n2) return false;
-    else return true;
+    return i===n1;
 };
