c6dce8ddcf83b094f0e771f8edc18cdca30ee22d
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-01-02 08:19:50 +0530

    Time: 5 ms (86.79%), Space: 85.6 MB (76.77%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,14 +1,14 @@
 class Solution {
+    HashSet<Character> vowel = new HashSet<>();
     public int[] vowelStrings(String[] words, int[][] queries) {
-        List<Character> vowel = new ArrayList<>();
         vowel.add('a'); vowel.add('e'); vowel.add('i'); vowel.add('o'); vowel.add('u');
         int[] vowelSum = new int[words.length];
         int[] ans = new int[queries.length];
         int index = 0;
-        if (startEndVowel(words[0], vowel)) vowelSum[0] = 1;
+        if (startEndVowel(words[0])) vowelSum[0] = 1;
         for (int i=1; i<words.length; i++){
             vowelSum[i] = vowelSum[i-1];
-            if (startEndVowel(words[i], vowel)) vowelSum[i]+=1;
+            if (startEndVowel(words[i])) vowelSum[i]+=1;
         }
         for (int[] range: queries){
             if (range[0]==0) ans[index++] = vowelSum[range[1]];
@@ -16,7 +16,7 @@ class Solution {
         }
         return ans;
     }
-    public boolean startEndVowel(String str, List<Character> vowel){
+    public boolean startEndVowel(String str){
         int n = str.length();
         return (vowel.contains(str.charAt(0)) && vowel.contains(str.charAt(n-1)));
     }
