73011c206e2d929bb83243c61fe2f77739f5b1b8
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-01-07 16:34:48 +0530

    Time: 4 ms (86.21%), Space: 42.2 MB (73.02%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -5,8 +5,8 @@ class Solution {
         Arrays.sort(words, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
         for (int i=0; i<n-1; i++){
             int j = i+1;
-            while (j<n && words[j].indexOf(words[i])==-1) j++;
-            if (j<n && words[j].indexOf(words[i])!=-1) ans.add(words[i]);
+            while (j<n && !words[j].contains(words[i])) j++;
+            if (j<n && words[j].contains(words[i])) ans.add(words[i]);
         }
         return ans;
     }
