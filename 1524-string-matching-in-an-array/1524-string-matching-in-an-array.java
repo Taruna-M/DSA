362302f7ea9d0b03f332e9aa05e2bf43c9553c38
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-01-07 16:31:12 +0530

    Time: 3 ms (92.21%), Space: 42.2 MB (73.02%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -2,13 +2,11 @@ class Solution {
     public List<String> stringMatching(String[] words) {
         int n = words.length;
         List<String> ans = new ArrayList<>();
-        Arrays.sort(words, (s1, s2) -> Integer.compare(s2.length(), s1.length()));
+        Arrays.sort(words, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
         for (int i=0; i<n-1; i++){
-            for (int j=i+1; j<n; j++){
-                if (!ans.contains(words[j])){
-                    if (words[i].indexOf(words[j])!=-1) ans.add(words[j]);
-                }
-            }
+            int j = i+1;
+            while (j<n && words[j].indexOf(words[i])==-1) j++;
+            if (j<n && words[j].indexOf(words[i])!=-1) ans.add(words[i]);
         }
         return ans;
     }
