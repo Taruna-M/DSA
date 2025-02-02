6dfbce3eeaee9b5a632d5ce629dffefe4aa11638
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-02-02 10:48:55 +0530

    Time: 1 ms (37.43%), Space: 43.3 MB (56.55%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean isArraySpecial(int[] nums) {
         int n = nums.length;
         for (int i=0; i<n-1; i++){
-            int sum = nums[i] + nums[i+1];
-            if (sum%2==0) return false;
+            if (nums[i]%2 == nums[i+1]%2) return false;
         }
         return true;
     }
