bfca6e0d37885ef182661c6f033982c4c064cdb0
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-02-05 23:05:53 +0530

    Time: 0 ms (100%), Space: 41.5 MB (76.92%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -4,13 +4,10 @@ class Solution {
         char[] str1 = s1.toCharArray();
         char[] str2 = s2.toCharArray();
         int index = -1;
-        char ch;
         int notSame = 0;
         for (int i = 0; i<n; i++){
             if (str1[i]!=str2[i]) {
-                if (notSame>0){
-                    if (str1[index]!=str2[i] || str1[i]!=str2[index]) return false;
-                }
+                if (notSame>0 && (str1[index]!=str2[i] || str1[i]!=str2[index])) return false;
                 else index = i;
                 notSame++;
                 if (notSame>2) return false;
