bd2b55d6bc18cd188b138fe652d37fa4ac8a3e47
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-02-13 06:00:42 +0530

    Time: 71 ms (24.87%), Space: 60.6 MB (54.88%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -2,21 +2,20 @@ class Solution {
     public int maximumSum(int[] nums) {
         int ans = -1, n = nums.length;
         int[] sums = new int[n];
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
         for (int i=0; i<n; i++){
             sums[i] = sumOfDigits(nums[i]);
         }
         for (int i=0; i<n; i++){
-            map.putIfAbsent(sums[i], new ArrayList<Integer>());
+            map.putIfAbsent(sums[i], new PriorityQueue<>(Collections.reverseOrder()));
             map.get(sums[i]).add(nums[i]);
         }
-        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
-            List<Integer> index = entry.getValue();
+        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : map.entrySet()) {
+            PriorityQueue<Integer> index = entry.getValue();
             if (index.size()>1){
-            Collections.sort(index);
-            int sum = 0;
-            sum+=index.get(index.size()-1)+index.get(index.size()-2);
-            ans = Math.max(sum,ans);
+                int sum = 0;
+                sum+=index.poll()+index.poll();
+                ans = Math.max(sum,ans);
             }
         }
         return ans;
