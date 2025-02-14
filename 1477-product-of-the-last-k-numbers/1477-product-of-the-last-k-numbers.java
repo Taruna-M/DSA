433a083a13738974073c728f2b44465aeef1a318
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-02-14 07:08:34 +0530

    Time: 15 ms (91.34%), Space: 69.1 MB (29.13%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
@@ -1,6 +1,6 @@
 class ProductOfNumbers {
     List<Integer> q = new ArrayList<>();
-    int n = 0;
+    int n = 1;
     public ProductOfNumbers() {
         q.add(1);
     }
@@ -9,17 +9,17 @@ class ProductOfNumbers {
         if (num==0){
             q = new ArrayList<>();
             q.add(1);
-            n = 0;
+            n = 1;
         }
         else {
-            int value = q.get(n);
+            int value = q.get(n-1);
             q.add(value*num);
             n++;
         }
     }
     
     public int getProduct(int k) {
-        if (k<=n) return q.get(n)/q.get(n-k);
+        if (k<n) return q.get(n-1)/q.get(n-1-k);
         return 0;
     }
 }
@@ -30,3 +30,4 @@ class ProductOfNumbers {
  * obj.add(num);
  * int param_2 = obj.getProduct(k);
  */
+
