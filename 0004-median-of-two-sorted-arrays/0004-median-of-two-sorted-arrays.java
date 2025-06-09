ed473dd53e2fe19ca05891a0e79ea6a6c7d87215
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-06-09 10:19:33 +0530

    Time: 1 ms (100%), Space: 46.1 MB (73.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,23 +1,32 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-        int[] arr = new int[n1+n2];
-        int i = 0, j = 0, k = 0;
-        while (i<n1 && j<n2){
-            if (nums1[i]<=nums2[j]) arr[k++] = nums1[i++];
-            else arr[k++] = nums2[j++];
-        } 
-        
-        while (i<n1){
-            arr[k++] = nums1[i++];
+        int[] A = nums1, B = nums2; 
+        if (A.length>B.length){ // A is always the smaller array
+            int[] temp = A;
+            A = B;
+            B = temp;
         }
+        int n1 = A.length, n2 = B.length;
+        int total = n1+n2, half = total / 2;
+        int low = 0, high = n1;
+        while (low<=high){
+            int midA = (low+high)/2;
+            int midB = half - midA; 
 
-        while (j<n2){
-            arr[k++] = nums2[j++];
+            int Aleft = midA>0 ? A[midA - 1] : Integer.MIN_VALUE;
+            int Aright = (midA)<n1 ? A[midA] : Integer.MAX_VALUE;
+            int Bleft = midB>0 ? B[midB - 1] : Integer.MIN_VALUE;
+            int Bright = (midB)<n2 ? B[midB] : Integer.MAX_VALUE;
+
+            if (Aleft<=Bright && Bleft<=Aright){
+                if (total%2 != 0)return Math.min(Aright, Bright);
+                else return (Math.max(Aleft, Bleft) + Math.min(Aright, Bright)) / 2.0;
+            }
+
+            else if (Aleft>Bright) high = midA - 1;
+            else if (Bleft>Aright) low = midA + 1;
         }
-
-        if ((n1+n2)%2 == 0) return (arr[((n1+n2)/2)-1] + arr[((n1+n2)/2)]) / 2.0;
-        else return (double) arr[(0+(n1+n2-1))/2];
+        return -1;
 
     }
 }
