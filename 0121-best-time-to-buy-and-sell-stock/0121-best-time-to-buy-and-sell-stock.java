7abcd27c7cfb6d158e8d581d91b8f4d6c497f614
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-06-10 20:20:14 +0530

    Time: 2 ms (75.29%), Space: 61.5 MB (82.49%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // sliding window, right var move when condition is good
+        // move left when condition is no longer satisfied
+        int profit = 0, n = prices.length;
+        int i = 0, j = 1;
+        while (j<n){
+            while (j<n && prices[j]>=prices[i]){
+                profit = Math.max(prices[j]-prices[i], profit);
+                j++;
+            }
+            if (j<n && prices[j]<prices[i]) i = j;
+            j++;
+        }
+        return profit;
+    }
+}
