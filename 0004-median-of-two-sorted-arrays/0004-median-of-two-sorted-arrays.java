485a36b4346e1e6a4033be3738cc4b0cf648443e
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-06-10 19:52:55 +0530

    Time: 1 ms (100%), Space: 46.3 MB (26.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,32 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] A = nums1, B = nums2; 
-        if (A.length>B.length){ // A is always the smaller array
+        int[] A = nums1, B = nums2;
+        if (A.length>B.length){
             int[] temp = A;
             A = B;
             B = temp;
         }
-        int n1 = A.length, n2 = B.length;
-        int total = n1+n2, half = total / 2;
+        int n1 = A.length, n2 = B.length, total = n1+n2, half = total/2;
         int low = 0, high = n1;
         while (low<=high){
-            int midA = (low+high)/2;
-            int midB = half - midA; 
+            int medA = (low+high)/2;
+            int medB = half - medA;
 
-            int Aleft = midA>0 ? A[midA - 1] : Integer.MIN_VALUE;
-            int Aright = (midA)<n1 ? A[midA] : Integer.MAX_VALUE;
-            int Bleft = midB>0 ? B[midB - 1] : Integer.MIN_VALUE;
-            int Bright = (midB)<n2 ? B[midB] : Integer.MAX_VALUE;
+            // get the left and right most/least values
+            int Aleft = medA>0 ? A[medA-1] : Integer.MIN_VALUE;
+            int Aright = medA<n1 ? A[medA] : Integer.MAX_VALUE;
+            int Bleft = medB>0 ? B[medB-1] : Integer.MIN_VALUE;
+            int Bright = medB<n2 ? B[medB] : Integer.MAX_VALUE;
 
             if (Aleft<=Bright && Bleft<=Aright){
-                if (total%2 != 0)return Math.min(Aright, Bright);
-                else return (Math.max(Aleft, Bleft) + Math.min(Aright, Bright)) / 2.0;
+                if (total%2==0) return (Math.min(Aright, Bright) + Math.max(Aleft, Bleft)) / 2.0;
+                else return Math.min(Aright, Bright);
             }
 
-            else if (Aleft>Bright) high = midA - 1;
-            else if (Bleft>Aright) low = midA + 1;
+            else if (Aleft>Bright){
+                high = medA - 1;
+            }
+            else low = medA + 1;
         }
         return -1;
-
     }
 }
