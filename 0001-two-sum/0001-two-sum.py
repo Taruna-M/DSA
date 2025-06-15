2ec00a1610bd98b14e3c6f752adc0971b0bb4b55
Taruna Mulugur <139420856+Taruna-M@users.noreply.github.com>
2025-06-16 00:12:38 +0530

    Time: 3 ms (50.65%), Space: 19.1 MB (24.16%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, num in enumerate(nums):
+            try:
+                return [d[target - num], i]
+            except KeyError:
+                d[num] = i
+
+        
